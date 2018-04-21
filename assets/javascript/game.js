09909b4c9abc703625e610725dedd7a1ec204c86
Juan Luis Hernandez <juanluishdz85@gmail.com>
2018-04-20 22:45:19 -0500

    Finished game with some flaws

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,14 +2,12 @@
 
 var wordBank = ["Australia", "Iran", "Japan", "Korea", "Egypt", "Morocco", "Nigeria", "Senegal", "Tunisia", 
  "Mexico", "Panama", "Argentina", "Brazil", "Colombia", "Peru", "Uruguay", "Belgium", "Croatia", "Denmark", "England",
- "France", "Germany", "Iceland", "Poland", "Portugal", "Russia", "Serbia", "Spain", "Sweden", "Switzerland"];
+ "France", "Germany", "Iceland", "Poland", "Portugal", "Russia", "Serbia", "Spain", "Sweden"];
 
  var randomTeam = wordBank[Math.floor(Math.random() * wordBank.length)];
 
-var wordChoice = randomTeam.toLowerCase().split("");
-console.log(wordChoice)
+var wordChoice = randomTeam.toUpperCase().split("");
 var underS;
-var count = 0;
 //empty array to store guesses
 var answerArray = [];
 var userGuess;
@@ -17,11 +15,10 @@ var lettersguessed = [];
 var guessleft = 10;
 var wins = 0;
 var losses = 0;
-
+var score = 0;
 //answer array with underscores
 function startUp() { 
-  for (var i = 0; i < randomTeam.length; i++)
-  {
+  for (var i = 0; i < randomTeam.length; i++){
     answerArray[i] = "_";
   }
   
@@ -30,31 +27,59 @@ function startUp() {
   document.querySelector(".wins").innerHTML = wins;
   document.querySelector(".losses").innerHTML = losses;
   document.querySelector(".guessesleft").innerHTML = guessleft;
+  }
 
+  //Here is where I tried to make my reset button work but it messed up other things so I did not apply it.
+  // function reset() {
+  //   randomTeam = wordBank[Math.floor(Math.random() * wordBank.length)];
+  //   console.log(randomTeam);
+  //   console.log(wordChoice);
+  //   for (var i = 0; i < wordChoice.length; i++){
+     
+  //     underS = answerArray.join(" ");
+  //     console.log(underS);
+
+  //   }
+  //   document.querySelector(".answer").innerHTML = underS;
+  //   guessleft = 10;
+  //   lettersguessed = [];
+  //   document.querySelector(".lettersg").innerHTML = lettersguessed;
+  //   document.querySelector(".guessesleft").innerHTML = guessleft;
+  //   }
+  // Used this function to reload the page since the above did not work
+  function reset (){
+    location.reload();
   }
   document.onkeyup = function(event){
-  userGuess = event.key.toLowerCase();
+  userGuess = event.key.toUpperCase();
   lettersguessed.push(userGuess);
-  console.log(lettersguessed);
     
     document.querySelector(".lettersg").innerHTML = lettersguessed;
     guessleft--;  //decreases the amount of guesses left
     document.querySelector(".guessesleft").innerHTML = guessleft;
 
-    
+  //If the user guesses a letter from the word, that letter will replace the underscore
   for (var i = 0; i < wordChoice.length; i++){
-    console.log(wordChoice[i]);
     if (wordChoice[i] === userGuess){
-      console.log(wordChoice)
       answerArray[i] = userGuess;
-
+      score++;
+      }   
     }
-  
-}
-  document.querySelector(".answer").innerHTML = answerArray.join(" ");
+   if (wordChoice.length === score) {
+     wins++;
+   }
+
+   if (guessleft === 0){
+     losses++;
+   }
+   
+   
+//updates the answer field with letters guessed and records wins
+document.querySelector(".answer").innerHTML = answerArray.join(" ");
+document.querySelector(".wins").innerHTML = wins;
+document.querySelector(".losses").innerHTML = losses;
+
 
   }
 
-  console.log(randomTeam); 
   startUp();
-
